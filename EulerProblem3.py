d5500b9e64f75198c38dc9cc7b759bf10a758ce1
nahid <nahid.seidi@gmail.com>
2020-07-24 23:22:31 +0200

    Cosmetic changes

--- EulerProblem3.py
+++ EulerProblem3.py
@@ -3,13 +3,11 @@ import numpy
 
 number = 600851475143
 primeFactors = []
-sqrt = math.sqrt(number)
 while number % 2 == 0:
-	number = number/2
+	number = number / 2
 	primeFactors.append(2)
-for i in numpy.arange (3, sqrt, 2):
-	while number%i==0:
+for i in numpy.arange (3, math.sqrt(number), 2):
+	while number % i == 0:
 		primeFactors.append(i)
-		number = number/i
+		number = number / i
 print(primeFactors[-1])
-#print(list(dict.fromkeys(primeFactors)))
